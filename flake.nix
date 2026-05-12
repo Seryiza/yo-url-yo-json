b27a7cfc4121fbde3fc7403784ba8824dd1f23e3
Sergey Zaborovsky <git@seryiza.xyz>
2026-05-12 08:56:43 +0600

    fix: use cloakbrowser from nix flakes

--- flake.nix
+++ flake.nix
@@ -2,11 +2,15 @@
   description = "Development shell for yo-url-yo-json";
 
   inputs = {
+    cloakbrowser = {
+      url = "github:Seryiza/CloakBrowser";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { cloakbrowser, nixpkgs, ... }:
     let
       lib = nixpkgs.lib;
       systems = [
@@ -20,54 +24,21 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          browserRuntimeLibs = with pkgs; [
-            alsa-lib
-            at-spi2-atk
-            at-spi2-core
-            atk
-            cairo
-            cups
-            dbus
-            expat
-            fontconfig
-            freetype
-            gdk-pixbuf
-            glib
-            gtk3
-            libdrm
-            libGL
-            libx11
-            libxkbcommon
-            libxcomposite
-            libxdamage
-            libxext
-            libxfixes
-            libxrandr
-            libxscrnsaver
-            libxcb
-            libxshmfence
-            mesa
-            nspr
-            nss
-            pango
-            stdenv.cc.cc
-            zlib
-          ];
+          cloakbrowserChromium = cloakbrowser.packages.${system}.cloakbrowserChromium;
         in
         {
           default = pkgs.mkShell {
             packages =
               (with pkgs; [
                 bun
-                cacert
                 git
                 nodejs_22
               ])
-              ++ browserRuntimeLibs;
+              ++ [
+                cloakbrowserChromium
+              ];
 
-            LD_LIBRARY_PATH = lib.makeLibraryPath browserRuntimeLibs;
-            SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-
+            CLOAKBROWSER_BINARY_PATH = "${cloakbrowserChromium}/bin/cloakbrowser-chrome";
             shellHook = ''
               export CLOAKBROWSER_CACHE_DIR="$PWD/.yo-url-yo-json/cloakbrowser"
               mkdir -p "$CLOAKBROWSER_CACHE_DIR"
